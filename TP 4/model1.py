5574d1a0c31afbefd059807a55975ba95880fc1c
felipegodi <felipegarciavassallo@gmail.com>
2022-07-21 20:20:51 -0300

    cambié model1 y model3
    Me dí cuenta que estaban desordenados

--- TP 4/model1.py
+++ TP 4/model1.py
@@ -1,4 +1,10 @@
-#Importo los paquetes necesarios para model1
+"""
+Model exported as python.
+Name : model1
+Group : 
+With QGIS : 32208
+"""
+
 from qgis.core import QgsProcessing
 from qgis.core import QgsProcessingAlgorithm
 from qgis.core import QgsProcessingMultiStepFeedback
@@ -13,7 +19,6 @@ class Model1(QgsProcessingAlgorithm):
         self.addParameter(QgsProcessingParameterFeatureSink('Wldsout', 'wldsout', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Length', 'length', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Field_calc', 'field_calc', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
-        self.addParameter(QgsProcessingParameterFeatureSink('Output_menor_a_11', 'OUTPUT_menor_a_11', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, defaultValue=None))
         self.addParameter(QgsProcessingParameterFeatureSink('Fix_geo', 'fix_geo', type=QgsProcessing.TypeVectorAnyGeometry, createByDefault=True, supportsAppend=True, defaultValue=None))
 
     def processAlgorithm(self, parameters, context, model_feedback):
@@ -22,56 +27,19 @@ class Model1(QgsProcessingAlgorithm):
         feedback = QgsProcessingMultiStepFeedback(6, model_feedback)
         results = {}
         outputs = {}
-        
-        #######################################################################
-        # Feature filter
-        #######################################################################
-        # Selecciono solo a las observaciones que tienen menos de 11 caracteres
-        # Esto no va después de crear length?
-        alg_params = {
-            'INPUT': 'Calculated_44855414_7d2b_42b4_abe3_b40f31f9bd4c',
-            'OUTPUT_menor_a_11': parameters['Output_menor_a_11']
-        }
-        outputs['FeatureFilter'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Output_menor_a_11'] = outputs['FeatureFilter']['OUTPUT_menor_a_11']
 
-        feedback.setCurrentStep(1)
-        if feedback.isCanceled():
-            return {}
-
-        #######################################################################
-        # Field calculator
-        #######################################################################
-        # Creo una columna con el largo de la columna "NAME_PROP" solo para 
-        # los que tienen menos de 11 caracteres
-        alg_params = {
-            'FIELD_LENGTH': 2,
-            'FIELD_NAME': 'length',
-            'FIELD_PRECISION': 0,
-            'FIELD_TYPE': 1,  # Integer
-            'FORMULA': 'length(NAME_PROP)',
-            'INPUT': 'Incremented_629f3621_675f_4244_a3e1_552d3045917d',
-            'OUTPUT': parameters['Length']
-        }
-        outputs['FieldCalculator'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Length'] = outputs['FieldCalculator']['OUTPUT']
-
-        feedback.setCurrentStep(2)
-        if feedback.isCanceled():
-            return {}
-        
         #######################################################################
         # Fix geometries
         #######################################################################
         # Arreglar la geometrías para procesar el shapefile
         alg_params = {
-            'INPUT': 'G:/Mi unidad/UdeSA Maestria en Economia/Segundo Trimestre/Herramientas/Clase 4/input/langa/langa.shp',
+            'INPUT': 'C:/Users/felip/Documents/UdeSA/Maestría/Herramientas computacionales/Clase 4/Input/langa/langa.shp',
             'OUTPUT': parameters['Fix_geo']
         }
         outputs['FixGeometries'] = processing.run('native:fixgeometries', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Fix_geo'] = outputs['FixGeometries']['OUTPUT']
 
-        feedback.setCurrentStep(3)
+        feedback.setCurrentStep(1)
         if feedback.isCanceled():
             return {}
 
@@ -83,7 +51,7 @@ class Model1(QgsProcessingAlgorithm):
             'FIELD_NAME': 'GID',
             'GROUP_FIELDS': [''],
             'INPUT': outputs['FixGeometries']['OUTPUT'],
-            'MODULUS': None,
+            'MODULUS': 0,
             'SORT_ASCENDING': True,
             'SORT_EXPRESSION': '',
             'SORT_NULLS_FIRST': False,
@@ -93,23 +61,42 @@ class Model1(QgsProcessingAlgorithm):
         outputs['AddAutoincrementalField'] = processing.run('native:addautoincrementalfield', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Autoinc_id'] = outputs['AddAutoincrementalField']['OUTPUT']
 
-        feedback.setCurrentStep(4)
+        feedback.setCurrentStep(2)
         if feedback.isCanceled():
             return {}
 
         #######################################################################
-        # Drop field(s)
+        # Field calculator
         #######################################################################
-        # Dropeo columnas que no voy a usar
+        # Creo una columna con el largo de la columna "NAME_PROP" solo para
+        # poder seleccionar según el número de caracteres
         alg_params = {
-            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length'],
-            'INPUT': 'Calculated_f9812dca_8e69_41df_bef2_fbb13213c1a3',
-            'OUTPUT': parameters['Wldsout']
+            'FIELD_LENGTH': 2,
+            'FIELD_NAME': 'length',
+            'FIELD_PRECISION': 0,
+            'FIELD_TYPE': 1,  # Integer
+            'FORMULA': 'length(NAME_PROP)',
+            'INPUT': outputs['AddAutoincrementalField']['OUTPUT'],
+            'OUTPUT': parameters['Length']
         }
-        outputs['DropFields'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
-        results['Wldsout'] = outputs['DropFields']['OUTPUT']
+        outputs['FieldCalculator'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Length'] = outputs['FieldCalculator']['OUTPUT']
 
-        feedback.setCurrentStep(5)
+        feedback.setCurrentStep(3)
+        if feedback.isCanceled():
+            return {}
+
+        #######################################################################
+        # Feature filter
+        #######################################################################
+        # Selecciono solo a las observaciones que tienen menos de 11 caracteres
+        alg_params = {
+            'INPUT': outputs['FieldCalculator']['OUTPUT'],
+            'OUTPUT_menor_a_11': QgsProcessing.TEMPORARY_OUTPUT
+        }
+        outputs['FeatureFilter'] = processing.run('native:filter', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+
+        feedback.setCurrentStep(4)
         if feedback.isCanceled():
             return {}
 
@@ -117,18 +104,34 @@ class Model1(QgsProcessingAlgorithm):
         # Field calculator clone
         #######################################################################
         # Creo la columna lnm con "NAME_PROP" solo para las observaciones que
-        #tienen menos de 11 caracteres
+        # tienen menos de 11 caracteres
         alg_params = {
             'FIELD_LENGTH': 10,
             'FIELD_NAME': 'lnm',
             'FIELD_PRECISION': 0,
             'FIELD_TYPE': 2,  # String
             'FORMULA': '"NAME_PROP"',
-            'INPUT': 'menor_a_11_6eb712fc_f069_4df4_b6c2_560f31419b32',
+            'INPUT': outputs['FeatureFilter']['OUTPUT_menor_a_11'],
             'OUTPUT': parameters['Field_calc']
         }
         outputs['FieldCalculatorClone'] = processing.run('native:fieldcalculator', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
         results['Field_calc'] = outputs['FieldCalculatorClone']['OUTPUT']
+
+        feedback.setCurrentStep(5)
+        if feedback.isCanceled():
+            return {}
+
+        #######################################################################
+        # Drop field(s)
+        #######################################################################
+        # Dropeo columnas que no uso
+        alg_params = {
+            'COLUMN': ['ID_ISO_A3','ID_ISO_A2','ID_FIPS','NAM_LABEL','NAME_PROP','NAME2','NAM_ANSI','CNT','C1','POP','LMP_POP1','G','LMP_CLASS','FAMILYPROP','FAMILY','langpc_km2','length'],
+            'INPUT': outputs['FieldCalculatorClone']['OUTPUT'],
+            'OUTPUT': parameters['Wldsout']
+        }
+        outputs['DropFields'] = processing.run('native:deletecolumn', alg_params, context=context, feedback=feedback, is_child_algorithm=True)
+        results['Wldsout'] = outputs['DropFields']['OUTPUT']
         return results
 
     def name(self):
